85f463bb4ed587d2528c908c5009264c5826092a
Kufieta-Jakub <kufietajakub@gmail.com>
2025-04-14 13:28:02 +0200

    first commit

---
+++ Backend/brainlex/src/main/java/com/example/brainlex/BrainlexApplication.java
@@ -0,0 +1,13 @@
+package com.example.brainlex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrainlexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrainlexApplication.class, args);
+	}
+
+}
